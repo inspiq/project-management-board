3ea22681df27376053cf179f44711d800260d346
inspiq <inspiqw@gmail.com>
2023-09-28 13:50:35 +0400

    feat: basic project setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from 'app/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
-)
+root.render(<App></App>)
